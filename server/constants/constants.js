6406da0c7f44e1cffd2cee8c1de5f6460542b71f
서종대 <jongdae94@naver.com>
2022-08-24 11:05:30 +0900

    server In Progress

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -4,11 +4,12 @@ export default class Defs {
 
     static NAME_DB_USER = 'Users'
     static DEFINITION_DB_USER = {
-        id: { type: Number, maxlength: 1000, unique: 1 },  // 자동으로 들어가게 어떻게??
+        id: { type: Number, maxlength: 1000, unique: 1 },
         userid: { type: String, maxlength: 50, unique: 1 },
         password: { type: String, maxlength: 50 },
         nickname: { type: String, maxlength: 50, unique: 1 },
         address: { type: String, maxlength: 500 },
+        privateKey: { type: String, maxlength: 500 }, // 추가
         token_amount: { type: Number, maxlength: 100 },
         eth_amoint: { type: Number, maxlength: 100 },
         created_at: { type: Date, maxlength: 100 },
@@ -17,7 +18,7 @@ export default class Defs {
 
     static NAME_DB_POST = 'Post'
     static DEFINITION_DB_POST = {
-        id: { type: Number, maxlength: 1000, unique: 1 },  // 자동으로 들어가게 어떻게??
+        id: { type: Number, maxlength: 1000, unique: 1 },
         userid: { type: String, maxlength: 50 },
         nickname: { type: String, maxlength: 50 },
         title: { type: String, maxlength: 500 },
